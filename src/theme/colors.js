4bc94a5f0ef4854951d0749348021bce97fe563d
filtcgil <archiviofiltcgil@gmail.com>
2026-01-16 18:20:17 +0100

    Update colors.js

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -1,7 +1,6 @@
 export const COLORS = {
-  primary: '#C62828', // rosso sindacale
-  secondary: '#FFFFFF',
-  background: '#F5F5F5',
-  text: '#212121',
-  gray: '#9E9E9E',
+  primary: "#1E90FF",
+  gray: "#A9A9A9",
+  lightGray: "#E0E0E0",
+  text: "#333"
 };
