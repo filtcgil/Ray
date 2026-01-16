d06d814ba6455054c974c3100b8e0f697f216b65
filtcgil <archiviofiltcgil@gmail.com>
2026-01-16 17:58:27 +0100

    Update HomeDriveScreen.js

--- src/screens/HomeDriveScreen.js
+++ src/screens/HomeDriveScreen.js
@@ -1,71 +1,26 @@
-import React, { useState } from 'react';
-import { View, FlatList, StyleSheet, TextInput } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, FlatList } from 'react-native';
 import DriveItem from '../components/DriveItem';
-import DriveBreadcrumb from '../components/DriveBreadcrumb';
-import { mockDrive } from '../data/mockDrive';
-import { COLORS } from '../theme/colors';
+import axios from 'axios';
 
 export default function HomeDriveScreen() {
-  const [currentFolder, setCurrentFolder] = useState('root');
-  const [path, setPath] = useState([{ id: 'root', name: 'Archivio' }]);
-  const [search, setSearch] = useState('');
+  const [folders, setFolders] = useState([]);
 
-  const data = mockDrive[currentFolder] || [];
-
-  const filteredData = data.filter(item =>
-    item.name.toLowerCase().includes(search.toLowerCase())
-  );
-
-  const openItem = (item) => {
-    if (item.type === 'folder') {
-      setCurrentFolder(item.id);
-      setPath([...path, { id: item.id, name: item.name }]);
-      setSearch('');
-    } else {
-      alert(`Apri file: ${item.name}`);
-    }
-  };
-
-  const navigateBreadcrumb = (index) => {
-    const newPath = path.slice(0, index + 1);
-    setPath(newPath);
-    setCurrentFolder(newPath[newPath.length - 1].id);
-    setSearch('');
-  };
+  useEffect(() => {
+    axios.get('https://<YOUR_BACKEND_URL>/drive/folders')
+      .then(res => setFolders(res.data))
+      .catch(err => console.error(err));
+  }, []);
 
   return (
-    <View style={styles.container}>
-      <DriveBreadcrumb path={path} onNavigate={navigateBreadcrumb} />
-
-      <TextInput
-        placeholder="Cerca file o cartelle"
-        value={search}
-        onChangeText={setSearch}
-        style={styles.search}
-      />
-
+    <View style={{ flex: 1, padding: 16 }}>
       <FlatList
-        data={filteredData}
-        keyExtractor={(item) => item.id}
+        data={folders}
+        keyExtractor={item => item.id}
         renderItem={({ item }) => (
-          <DriveItem item={item} onPress={() => openItem(item)} />
+          <DriveItem item={item} onPress={() => console.log(item.name)} />
         )}
       />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: COLORS.background,
-  },
-  search: {
-    borderWidth: 1,
-    borderColor: COLORS.lightGray,
-    borderRadius: 8,
-    padding: 10,
-    marginBottom: 12,
-  },
-});
